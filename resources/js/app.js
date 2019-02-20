100a5e7d9faf8065dd91196a90e5176644ab2cc1
Alex Nevpryaga <alexnevpryaga@gmail.com>
2019-02-20 13:39:57 +0200

    fix ui

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,50 +5,74 @@ import 'jquery-typeahead/src/jquery.typeahead';
 export default (function () {
 
     $(document).on('click', '#add-row', function() {
-        $('li:last-child').clone().appendTo( "ul" );
+
+        console.log($('li.guest-data').length);
+        console.log($('li.guest-data').is('hidden'));
+
+        if ($('li.guest-data').length === 1 && $('li.guest-data').is('hidden')) {
+            return $('li.guest-data').show();
+        }
+        var cloned = $('li.guest-data:last-child').clone();
+        $('input', cloned).val('');
+        initAutosugest(cloned);
+        cloned.appendTo( "ul" );
+        cloned.show();
     });
 
-    $.typeahead({
-        input: '.js-typeahead-user_v1',
-        minLength: 1,
-        order: "asc",
-        dynamic: true,
-        delay: 500,
-        template: function (query, item) {
-            return '<span class="name">{{name}}</span>'
-        },
-        source: {
-            user: {
-                display: "name",
-                ajax: function (query) {
-                    return {
-                        type: "GET",
-                        url: "/api/search/",
-                        path: "response",
-                        data: {
-                            q: "{{query}}"
+    $(document).on('click', '.remove-row', function(event) {
+        event.preventDefault();
+        var parent = $(this).closest('li');
+        if ($('li.guest-data').length < 2) {
+            return parent.hide();
+        }
+        $(this).closest('li').remove();
+    });
+
+    var initAutosugest = function(elem) {
+        if (!elem) {
+            elem = $(document);
+        }
+        if (!elem.hasClass('js-typeahead-user')) {
+            elem = $('.js-typeahead-user', elem);
+        }
+        elem.typeahead({
+            minLength: 1,
+            order: "asc",
+            dynamic: true,
+            delay: 500,
+            template: function (query, item) {
+                return '<span class="name">{{name}}</span>'
+            },
+            source: {
+                user: {
+                    display: "name",
+                    ajax: function (query) {
+                        return {
+                            type: "GET",
+                            url: "/api/search/",
+                            path: "response",
+                            data: {
+                                q: "{{query}}"
+                            }
                         }
                     }
-                }
 
+                },
             },
-        },
-        callback: {
-            onClick: function (node, a, item, event) {
-                console.log(item)
-                $(node).closest('li').find('.phone-input').val(item.phone || '');
+            callback: {
+                onClick: function (node, a, item, event) {
+                    console.log(item)
+                    $(node).closest('li').find('.phone-input').val(item.phone || '');
+                }
             },
-            onSendRequest: function (node, query) {
-                console.log('request is sent')
+            selector: {
+                list: 'typeahead__list mt-0'
             },
-            onReceiveRequest: function (node, query) {
-                console.log('request is received')
-            }
-        },
-        selector: {
-            list: 'typeahead__list mt-0'
-        },
-        debug: true
-    });
+            debug: true
+        });
+
+    };
+    initAutosugest();
+
 
 })();
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,61 +3,103 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Please, fill the form</title>
-
-        <!-- Fonts -->
         <link href="/css/app.css" rel="stylesheet" type="text/css">
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
     </head>
     <body>
         <div class="container">
             <div class="mt-5 justify-content-md-center">
-                <div class="card">
-                    <div class="card-header">Please, fill the form</div>
-                    <form>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">
-                                    <div class="form-group row">
-                                        <label for="name" class="col-sm-2 col-form-label">Name</label>
-                                        <div class="col-sm-10 typeahead__container">
-                                            <div class="typeahead__container">
-                                                <div class="typeahead__field">
-                                                    <div class="typeahead__query">
-                                                        <input
-                                                                required
-                                                                class="js-typeahead-user_v1"
-                                                                name="name"
-                                                                type="search"
-                                                                placeholder="Name"
-                                                                autocomplete="off"
-                                                        >
+                <form>
+                    <div class="card">
+                        <div class="card-header">Please, fill the form</div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item pb-0 leader-data">
+                                    <div class="d-flex">
+                                        <div class="p-2 w-75">
+                                            <div class="form-group row">
+                                                <label for="leader-name" class="col-sm-2 col-form-label">Your Name</label>
+                                                <div class="col-sm-10 typeahead__container">
+                                                    <div class="typeahead__container">
+                                                        <div class="typeahead__field">
+                                                            <div class="typeahead__query">
+                                                                <input
+                                                                        id="leader-name"
+                                                                        required
+                                                                        class="js-typeahead-user form-control"
+                                                                        name="leader"
+                                                                        type="search"
+                                                                        placeholder="Name"
+                                                                        autocomplete="off"
+                                                                >
+                                                            </div>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
+                                            <div class="form-group row">
+                                                <label for="phone" class="col-sm-2 col-form-label">Phone</label>
+                                                <div class="col-sm-10">
+                                                    <input
+                                                            name="phone"
+                                                            class="form-control"
+                                                            type="tel"
+                                                            required
+                                                            placeholder="Phone" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="p-2 flex-shrink-1 ml-auto"></div>
+                                    </div>
+
+                                </li>
+                                <li class="list-group-item pb-0 guest-data">
+                                    <div class="d-flex">
+                                        <div class="p-2 w-75">
+                                            <div class="form-group row">
+                                                <label for="guest-name" class="col-sm-2 col-form-label">Guest Name</label>
+                                                <div class="col-sm-10 typeahead__container">
+                                                    <div class="typeahead__container">
+                                                        <div class="typeahead__field">
+                                                            <div class="typeahead__query">
+                                                                <input
+                                                                        required
+                                                                        class="js-typeahead-user form-control"
+                                                                        name="guest[]"
+                                                                        type="search"
+                                                                        placeholder="Name"
+                                                                        autocomplete="off"
+                                                                >
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label for="phone" class="col-sm-2 col-form-label">Phone</label>
+                                                <div class="col-sm-10">
+                                                    <input
+                                                            name="guest-phone[]"
+                                                            class="form-control phone-input"
+                                                            type="tel"
+                                                            required
+                                                            placeholder="Phone" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="p-2 flex-shrink-1 ml-auto">
+                                            <a href="#" class="remove-row text-decoration-none font-weight-bold h3">x</a>
                                         </div>
                                     </div>
-                                    <div class="form-group row">
-                                        <label for="phone" class="col-sm-2 col-form-label">Phone</label>
-                                        <div class="col-sm-10">
-                                            <input
-                                                    name="phone"
-                                                    class="form-control"
-                                                    type="tel"
-                                                    required
-                                                    placeholder="Phone" />
-                                        </div>
-                                    </div>
-                            </li>
-                        </ul>
-                    </form>
-                    <div class="card-body d-flex justify-content-end">
-                        <button id="add-row" type="button" class="btn btn-success mr-0">Add row</button>
+                                </li>
+                            </ul>
+                        <div class="card-body d-flex justify-content-end">
+                            <button id="add-row" type="button" class="btn btn-success mr-0">Add row</button>
+                        </div>
+                        <div class="card-footer text-muted d-flex justify-content-end">
+                            <button id="submit" type="submit" class="btn btn-primary mr-0">Submit</button>
+                        </div>
                     </div>
-                    <div class="card-footer text-muted d-flex justify-content-end">
-                        <button id="submit" type="submit" class="btn btn-primary mr-0">Submit</button>
-                    </div>
-                </div>
+                </form>
             </div>
         </div>
 
